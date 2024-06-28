7319f884b5dd0caf65238bafd1e1c405b79998b9
Arhan Ansari <arhanansari2009@gmail.com>
2024-06-28 12:59:00 +0000

    Initial commit

---
+++ html/script.js
@@ -0,0 +1,67 @@
+var socket;
+var usernameInput
+var chatIDInput;
+var messageInput;
+var chatRoom;
+var dingSound;
+var messages = [];
+var delay = true;
+
+function onload(){
+  socket = io();
+  usernameInput = document.getElementById("NameInput");
+  chatIDInput = document.getElementById("IDInput");
+  messageInput = document.getElementById("ComposedMessage");
+  chatRoom = document.getElementById("RoomID");
+  dingSound = document.getElementById("Ding");
+
+  socket.on("join", function(room){
+    chatRoom.innerHTML = "Chatroom : " + room;
+  })
+
+  socket.on("recieve", function(message){
+    console.log(message);
+    const timestamp = new Date(); // Get the current timestamp
+    const formattedTimestamp = formatTimestamp(timestamp);
+    const messageWithTimestamp = `<span class="timestamp">${formattedTimestamp}</span> ${message}`; // Add timestamp to message
+
+    if (messages.length < 9){
+      messages.push(messageWithTimestamp);
+      dingSound.currentTime = 0;
+      dingSound.play();
+    }
+    else{
+      messages.shift();
+      messages.push(messageWithTimestamp);
+    }
+    for (i = 0; i < messages.length; i++){
+        document.getElementById("Message"+i).innerHTML = messages[i];
+        document.getElementById("Message"+i).style.color = "#303030";
+    }
+  })
+}
+
+function Connect(){
+  socket.emit("join", chatIDInput.value, usernameInput.value);
+}
+
+function Send(){
+  if (delay && messageInput.value.replace(/\s/g, "") != ""){
+    delay = false;
+    setTimeout(delayReset, 1000);
+    socket.emit("send", messageInput.value);
+    messageInput.value = "";
+  }
+}
+
+function delayReset(){
+  delay = true;
+}
+
+function formatTimestamp(timestamp) {
+  const date = new Date(timestamp);
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`; 
+}
